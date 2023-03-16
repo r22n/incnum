f809445b5495dfe4763e3533c20c149cbba575b5
r22n <r22n@github.com>
2023-03-16 21:36:53 +0900

    feat doc

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.seq = exports.next = void 0;
+/**
+ * put next index into 'current' and returns 'carry-up' position.
+ *
+ * the next index are unspecified if;
+ * - 'ranges.length' were smaller than 'current.length' or smaller than 0,
+ * - 'ranges' had 0
+ *
+ * the 'current' should not be 'const'/'readonly' and mutable.
+ *
+ * returned value of 'carry-up' represents position on 'current' nexts index.
+ *
+ * if all ranges has gone, 'carry-up' is -1.
+ */
 function next(current, ranges) {
     let cu = current.length - 1;
     for (; cu >= 0; cu--) {
@@ -14,6 +27,13 @@ function next(current, ranges) {
     return cu;
 }
 exports.next = next;
+/**
+ * enumerates all indexes starts from 'initial' to 'ranges'.
+ *
+ * all indexes is unspecified if 'initial' was on out of 'ranges'.
+ *
+ * @see next
+ */
 function seq(initial, ranges) {
     if (!ranges.length || ranges.some(x => x === 0)) {
         return [];
--- index.ts
+++ index.ts
@@ -1,3 +1,17 @@
+
+/**
+ * put next index into 'current' and returns 'carry-up' position.
+ * 
+ * the next index are unspecified if;
+ * - 'ranges.length' were smaller than 'current.length' or smaller than 0, 
+ * - 'ranges' had 0
+ * 
+ * the 'current' should not be 'const'/'readonly' and mutable.
+ * 
+ * returned value of 'carry-up' represents position on 'current' nexts index.
+ * 
+ * if all ranges has gone, 'carry-up' is -1.
+ */
 export function next(current: number[], ranges: number[]) {
     let cu = current.length - 1;
     for (; cu >= 0; cu--) {
@@ -10,6 +24,13 @@ export function next(current: number[], ranges: number[]) {
     return cu;
 }
 
+/**
+ * enumerates all indexes starts from 'initial' to 'ranges'.
+ * 
+ * all indexes is unspecified if 'initial' was on out of 'ranges'.
+ * 
+ * @see next
+ */
 export function seq(initial: number[], ranges: number[]) {
     if (!ranges.length || ranges.some(x => x === 0)) {
         return [];
